2f07496f696169bd61df47bda86294af4184af20
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2023-04-14 08:09:40 +0100

    Header exported

--- simpleComponents/layout/header/header.ts
+++ simpleComponents/layout/header/header.ts
@@ -1,21 +1,23 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const mobileMenuToggle = document.querySelector(".c-mobile-menu-toggle");
-  const headerElement = document.querySelector(".c-header");
+export const initHeader = () => {
+  document.addEventListener("DOMContentLoaded", () => {
+    const mobileMenuToggle = document.querySelector(".c-mobile-menu-toggle");
+    const headerElement = document.querySelector(".c-header");
 
-  mobileMenuToggle?.addEventListener("click", () => {
-    headerElement?.classList.toggle("c-header--open");
+    mobileMenuToggle?.addEventListener("click", () => {
+      headerElement?.classList.toggle("c-header--open");
 
-    const icon = mobileMenuToggle.querySelector("i");
-    if (icon) {
-      if (icon.classList.contains("fa-bars")) {
-        icon.classList.remove("fa-bars");
-        icon.classList.add("fa-xmark");
-        mobileMenuToggle.setAttribute("aria-label", "Close menu");
-      } else {
-        icon.classList.remove("fa-xmark");
-        icon.classList.add("fa-bars");
-        mobileMenuToggle.setAttribute("aria-label", "Open menu");
+      const icon = mobileMenuToggle.querySelector("i");
+      if (icon) {
+        if (icon.classList.contains("fa-bars")) {
+          icon.classList.remove("fa-bars");
+          icon.classList.add("fa-xmark");
+          mobileMenuToggle.setAttribute("aria-label", "Close menu");
+        } else {
+          icon.classList.remove("fa-xmark");
+          icon.classList.add("fa-bars");
+          mobileMenuToggle.setAttribute("aria-label", "Open menu");
+        }
       }
-    }
+    });
   });
-});
+};
