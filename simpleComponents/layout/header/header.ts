47afed6d5366bbb2b832c2fc69348b78d5d2f623
elliotlafave123 <44415567+elliotlafave123@users.noreply.github.com>
2023-04-16 19:20:46 +0100

    Edits to header, footer, form and container

--- simpleComponents/layout/header/header.ts
+++ simpleComponents/layout/header/header.ts
@@ -1,23 +1,54 @@
+function toggleMobileMenu(mobileMenuToggle, headerElement) {
+  headerElement.classList.toggle("c-header--open");
+
+  const icon = mobileMenuToggle.querySelector("i");
+  if (icon) {
+    if (icon.classList.contains("fa-bars")) {
+      icon.classList.remove("fa-bars");
+      icon.classList.add("fa-xmark");
+      mobileMenuToggle.setAttribute("aria-label", "Close menu");
+    } else {
+      icon.classList.remove("fa-xmark");
+      icon.classList.add("fa-bars");
+      mobileMenuToggle.setAttribute("aria-label", "Open menu");
+    }
+  }
+}
+
+function initMobileMenuToggle(headerElement) {
+  const mobileMenuToggle = document.querySelector(".c-mobile-menu-toggle");
+  if (mobileMenuToggle) {
+    mobileMenuToggle.addEventListener("click", function () {
+      toggleMobileMenu(mobileMenuToggle, headerElement);
+    });
+  }
+}
+
+function initHeaderHideOnScroll(headerElement) {
+  let lastScrollPosition = 0;
+
+  window.addEventListener("scroll", function () {
+    const currentScrollPosition = window.pageYOffset;
+
+    if (currentScrollPosition < lastScrollPosition) {
+      // Scrolling up
+      headerElement.classList.remove("c-header--hidden");
+    } else {
+      // Scrolling down
+      headerElement.classList.add("c-header--hidden");
+    }
+
+    lastScrollPosition = currentScrollPosition;
+  });
+}
+
 export const initHeader = () => {
-  document.addEventListener("DOMContentLoaded", () => {
-    const mobileMenuToggle = document.querySelector(".c-mobile-menu-toggle");
+  document.addEventListener("DOMContentLoaded", function () {
     const headerElement = document.querySelector(".c-header");
 
-    mobileMenuToggle?.addEventListener("click", () => {
-      headerElement?.classList.toggle("c-header--open");
-
-      const icon = mobileMenuToggle.querySelector("i");
-      if (icon) {
-        if (icon.classList.contains("fa-bars")) {
-          icon.classList.remove("fa-bars");
-          icon.classList.add("fa-xmark");
-          mobileMenuToggle.setAttribute("aria-label", "Close menu");
-        } else {
-          icon.classList.remove("fa-xmark");
-          icon.classList.add("fa-bars");
-          mobileMenuToggle.setAttribute("aria-label", "Open menu");
-        }
-      }
-    });
+    if (headerElement) {
+      initMobileMenuToggle(headerElement);
+      initHeaderHideOnScroll(headerElement);
+    }
   });
 };
